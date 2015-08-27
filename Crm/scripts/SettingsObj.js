4506e2f5f47c37dc5ed337442ac3d12727219a1a
Alex Kravchenko <akrav4@gmail.com>
2015-08-27 11:18:22 +0000

    V 1048 * увеличены размеры масштабируемой картинки в галерее

--- Crm/scripts/SettingsObj.js
+++ Crm/scripts/SettingsObj.js
@@ -10,7 +10,7 @@ function settingsObjInit(onSuccess) {
     settings = {};
     
     // версия приложения
-    settings.appVersion = 1045;
+    settings.appVersion = 1048;
     
     // признак выполнения приложения в симуляторе
     settings.simulator = (window.navigator.simulator === true);
